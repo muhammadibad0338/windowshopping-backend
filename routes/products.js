294ba3055a2c206b3e0c1f8ef3687fa36695e659
Muhammad Ibad <mibad0338@gmail.com>
2022-02-17 01:27:35 +0500

    update

--- routes/products.js
+++ routes/products.js
@@ -123,7 +123,7 @@ router.put(`/:id`, uploadOptions.single('image'), async (req, res) => {
         const product = await Product.findById(req.params.id);
         if (!product) return res.status(400).send('Invalid Product!');
 
-        const file = req.files
+        const file = req.file;
         let imagepath;
 
         if (file) {
